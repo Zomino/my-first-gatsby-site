52d96f506b22fafa94e5d961dfe173deb97ff0b5
Zou Minowa <zouminowa@gmail.com>
2022-02-26 11:13:59 +0000

    Finish Gatsby Tutorial Part 6

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import Layout from '../components/layout';
+import { graphql, Link } from 'gatsby';
+import Layout from '../../components/layout';
 
 function BlogPage({ data }) {
   return (
@@ -10,15 +9,16 @@ function BlogPage({ data }) {
         {
           data.allMdx.nodes.map((node) => (
             <article key={node.id}>
-              <h2>{node.frontmatter.title}</h2>
+              <h2>
+                <Link to={`/blog/${node.slug}`}>
+                  {node.frontmatter.title}
+                </Link>
+              </h2>
               <p>
                 Posted:
                 {' '}
                 {node.frontmatter.date}
               </p>
-              <MDXRenderer>
-                {node.body}
-              </MDXRenderer>
             </article>
           ))
         }
@@ -36,7 +36,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
